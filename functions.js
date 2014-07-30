3e6fed7c90e50f45beaeb0c09a43fcdac9344fe0
briantwalter <walter.brian@gmail.com>
2014-07-30 15:38:58 -0700

    kind of working :)

--- functions.js
+++ functions.js
@@ -15,30 +15,50 @@ function setoutput()
   // set starboard side to output
   var outright = new XMLHttpRequest();
   var urlright = baseurl + "/GPIO/" + gpioright + "/function/out";
-  //alert("DEBUG: starboard url is " + urlright );
   outright.open("POST", urlright, true);
   outright.send();
   // set port side to output
   var outleft = new XMLHttpRequest();
   var urlleft = baseurl + "/GPIO/" + gpioleft + "/function/out";
-  //alert("DEBUG: port url is " + urlleft );
   outleft.open("POST", urlleft, true);
   outleft.send();
 }
 function propstop(screw)
 {
-  //alert("DEBUG: got propstop " + screw.name);
+  switch( screw.name ) {
+    case "left":
+      var stopgpio = gpioleft
+      break;
+    case "right":
+      var stopgpio = gpioright
+      break;
+    default:
+      alert("Error in propstop, with " + screw.name )
+      break;
+  }
+  var stopreq = new XMLHttpRequest();
+  var stopurl = baseurl + "/GPIO/" + stopgpio + "/value/0";
+  stopreq.open("POST", stopurl, true);
+  stopreq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  stopreq.send();
 }
 function propgo(screw)
 {
   //alert("DEBUG: got propgo " + screw.name );
-}
-function nothing() 
-{
-  var xmlhttp = new XMLHttpRequest();
-  var url = "/lights";
-  var params = "switch=true";
-  xmlhttp.open("POST", url, true);
-  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xmlhttp.send(params);
+  switch( screw.name ) {
+    case "left":
+      var startgpio = gpioleft
+      break;
+    case "right":
+      var startgpio = gpioright
+      break;
+    default:
+      alert("Error in propstart, with " + screw.name )
+      break;
+  }
+  var startreq = new XMLHttpRequest();
+  var starturl = baseurl + "/GPIO/" + startgpio + "/value/1";
+  startreq.open("POST", starturl, true);
+  startreq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  startreq.send();
 }
