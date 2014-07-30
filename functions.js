c89c98940d96105b4fcfc3020a43230210fb2dc6
briantwalter <walter.brian@gmail.com>
2014-07-29 21:16:43 -0700

    first commit

---
+++ functions.js
@@ -0,0 +1,36 @@
+// client side javascript controls
+
+// configuration
+var baseurl = 'http://atlantic.lan.walternet.us:8000'
+var gpioright = '17'
+var gpioleft = '27'
+
+// logic
+function debug() 
+{
+  alert("DEBUG: loaded functions file");
+}
+function setoutput() 
+{
+  // set starboard side to output
+  var outright = new XMLHttpRequest();
+  var urlright = baseurl + "/GPIO/" + gpioright + "/function/out";
+  alert("DEBUG: starboard url is " + urlright );
+  outright.open("POST", urlright, true);
+  outright.send();
+  // set port side to output
+  var outleft = new XMLHttpRequest();
+  var urlleft = baseurl + "/GPIO/" + gpioleft + "/function/out";
+  alert("DEBUG: port url is " + urlleft );
+  outleft.open("POST", urlleft, true);
+  outleft.send();
+}
+function nothing() 
+{
+  var xmlhttp = new XMLHttpRequest();
+  var url = "/lights";
+  var params = "switch=true";
+  xmlhttp.open("POST", url, true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send(params);
+}
